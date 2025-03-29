9116945b9d1c3d0e25aa02e439706606da5e49dc
Mykyta <nikiromanen@gmail.com>
2025-03-29 23:16:16 +0100

    Add login screen

--- View/LoginScreen/login_screen.py
+++ View/LoginScreen/login_screen.py
@@ -1,10 +1,19 @@
 from kivymd.uix.screen import MDScreen
-from View.Panel.panel import Panel
+from View.ManagerScreen.manager_screen import ManagerScreen
+from kivymd.app import MDApp
 
 class LoginScreen(MDScreen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.panel = Panel()
 
-    def open_menu(self, menu_button):
-        self.panel.open_menu(menu_button)
+    def __init__(self, manager_screen=None, **kwargs):
+        super().__init__(**kwargs)
+        self._user_logged_in = False
+        self.manager_screen = ManagerScreen()
+        self.manager_screen = manager_screen or MDApp.get_running_app().manager_screen
+
+    @property
+    def user_logged_in(self):
+        return self._user_logged_in
+
+    def login_success(self):
+        self._user_logged_in = True
+        self.manager_screen.switch_screen("menu")
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.core.window import Window
 from kivymd.app import MDApp
 from View.ManagerScreen.manager_screen import ManagerScreen
 from View.screens import screens
+from View.LoginScreen.login_screen import LoginScreen
 
 class CriticHub(MDApp):
     def __init__(self, **kwargs):
@@ -13,17 +14,19 @@ class CriticHub(MDApp):
         self.theme_cls.theme_style_switch_animation = True
         self.theme_cls.theme_style_switch_animation_duration = 0.8
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Darkblue"
+        self.theme_cls.primary_palette = "BlueGray"
 
         self.manager_screen = ManagerScreen()
+        self.login_screen = LoginScreen()
 
     def build(self):
         for screen_name, (module_path, class_name) in screens.items():
             self.manager_screen.load_screen(module_path, class_name, screen_name)
-        self.manager_screen.switch_screen("menu")
+
+        initial_screen = "login" if not self.login_screen.user_logged_in else "menu"
+        self.manager_screen.switch_screen(initial_screen)
 
         return self.manager_screen
-
 
 if __name__ == "__main__":
     CriticHub().run()
