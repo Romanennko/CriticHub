5e96ce51db8522af9b260309a09b45cf5011f4f4
Mykyta <nikiromanen@gmail.com>
2025-04-01 00:37:37 +0200

    Created basic functions for a table with users

--- database.py
+++ database.py
@@ -35,3 +35,34 @@ def create_user(username, password):
     finally:
         cur.close()
         conn.close()
+
+
+def check_credentials(username, password):
+    try:
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("SELECT password FROM users WHERE username = %s", (username,))
+        result = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        if result:
+            hashed_password = result[0].encode()
+            return bcrypt.checkpw(password.encode(), hashed_password)
+
+        return False
+
+    except Exception:
+        return False
+
+def check_user_exists(username):
+    try:
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT 1 FROM users WHERE username = %s", (username,))
+        result = cur.fetchone()
+        cur.close()
+        conn.close()
+        return result is not None
+    except Exception:
+        return False
